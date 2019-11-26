320ba450e077bc8c46d3101b1865539554a5b5ae
Rahat Ibn Rafiq <rahatibnrafiq@Rahats-MacBook-Pro.local>
2019-11-25 18:44:11 -0800

    changing title pages' texts

--- 2020/includes/title3.js
+++ 2020/includes/title3.js
@@ -1,16 +1,16 @@
 document.write("<table style=\"width:100%\">");
 document.write("<tr>");
 document.write("<td >	");
-document.write("<a target=\"_tab\" href=\"https://www2019.thewebconf.org/\" ><img src=\"../../figures/TheWebconf_logo.png\" width=\"100%\"/></a>");
+document.write("<a target=\"_tab\" href=\"https://www2020.thewebconf.org/\" ><img src=\"../../figures/TheWebconf_logo.png\" width=\"100%\"/></a>");
 document.write("</td>");
 
 
 document.write("<td align=\"center\">");
-document.write("<h1>CyberSafety 2019</h1>");
-document.write("<h3>The Fourth Workshop on Computational Methods in Online Misbehavior</h3>");
-document.write("<h3>Co-located with <a target=\"_tab\" href=\"https://www2019.thewebconf.org/\" >The Web Conference 2019</a> </h3>");
-document.write("<h3>May 13, 2019</h3>");
-document.write("<h3>San Francisco, California, USA</h3>");
+document.write("<h1>CyberSafety 2020</h1>");
+document.write("<h3>The Fifth Workshop on Computational Methods in Online Misbehavior</h3>");
+document.write("<h3>Co-located with <a target=\"_tab\" href=\"https://www2020.thewebconf.org/\" >The Web Conference 2020</a> </h3>");
+document.write("<h3>April 20-24, 2020</h3>");
+document.write("<h3>Taipei</h3>");
 
 document.write("</td>");
 document.write("<td>");
