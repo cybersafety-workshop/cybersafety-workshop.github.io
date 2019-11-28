c22629bfc15f3ee1e9ba4abbe24141147721f980
Rahat Ibn Rafiq <rahatibnrafiq@Rahats-MacBook-Pro.local>
2019-11-28 10:37:55 -0800

    added 2019

---
+++ 2019/includes/menu3.js
@@ -0,0 +1,24 @@
+document.write("<table class=\"menu\">");
+document.write("<tr>");
+document.write("<td>");
+document.write("<nav class=\"main-nav\">");
+document.write("<ul class=\"main-nav-ul\">");
+document.write("<li><a href=\"../../\">Home</a></li>");
+ddocument.write("<li><a href=\"../../committees\">Committees</a></li>");
+document.write("<li><a href=\"../../call_for_contributions\">Call For Contributions</a></li>");
+document.write("<li><a href=\"../../program\">Program</a></li>");
+document.write("<li><a href=\"../../important_dates\">Important Dates</a></li>");
+document.write("<li><a href=\"../../registration\">Registration</a></li>");
+document.write("<li><a href=\"../../travel\">Travel</a></li>");
+document.write("<li><a href=\"../../previous_workshops\">Previous Workshops</a></li>");
+document.write("<li><a href=\"../../pictures\">Pictures</a></li>");
+document.write("<li><a href=\"../../welcome_message\">Chairs' Welcome Message</a></li>");
+document.write("<li><a href=\"../../sponsors\">Sponsors</a></li>");
+document.write("<li><a href=\"../../awards\">Awards</a></li>");
+document.write("</ul>");
+document.write("</nav>");
+
+document.write("</td>");
+
+document.write("</tr>");
+document.write("</table>");
