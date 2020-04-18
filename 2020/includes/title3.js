4fdc7ced77195484d32f2700268c1996bc510115
Rahat Ibn Rafiq <rahatibnrafiq@Rahats-MacBook-Pro.local>
2020-04-17 19:41:50 -0700

    added april 21 as date, updated welcome message

--- 2020/includes/title3.js
+++ 2020/includes/title3.js
@@ -9,7 +9,7 @@ document.write("<td align=\"center\">");
 document.write("<h1>CyberSafety 2020</h1>");
 document.write("<h3>The Fifth Workshop on Computational Methods in Online Misbehavior</h3>");
 document.write("<h3>Co-located with <a target=\"_tab\" href=\"https://www2020.thewebconf.org/\" >The Web Conference 2020</a> </h3>");
-document.write("<h3>April 20-24, 2020</h3>");
+document.write("<h3>April 21, 2020</h3>");
 document.write("<h3>Taipei</h3>");
 
 document.write("</td>");
